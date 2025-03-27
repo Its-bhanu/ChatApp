83d97126da552b3ad9d7dae95999f2f29c83d1da
Its-bhanu <bhanu.22it498@rtu.ac.in>
2025-03-27 13:46:41 +0530

    push

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -3,7 +3,7 @@ import { create } from "zustand";
 export const useThemeStore = create((set) => ({
   theme: localStorage.getItem("chat-theme") || "light",
   setTheme: (theme) => {
-    localStorage.setItem("chat-theme", theme);
+    Storage.setItem("chat-theme", theme);
     set({ theme });
   },
 }));

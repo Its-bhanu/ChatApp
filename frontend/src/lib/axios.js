86025f214ef22a14230fb4f32dd3549c1adee4d7
Its-bhanu <bhanu.22it498@rtu.ac.in>
2025-03-28 10:23:32 +0530

    new

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chat-app-sxko.vercel.app/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chat-app-sxko.vercel.app" : "/api",
   withCredentials: true,
 });
